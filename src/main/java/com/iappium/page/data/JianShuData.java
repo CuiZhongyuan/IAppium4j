3b37485a3f2643cdf366db2fc46c0035928287d4
rootczy <365450906@qq.com>
2020-09-28 15:20:26 +0800

    增加演示示例--简书APP

---
+++ src/main/java/com/iappium/page/data/JianShuData.java
@@ -0,0 +1,11 @@
+package com.iappium.page.data;
+
+import org.openqa.selenium.By;
+
+public class JianShuData {
+    public static final By POPUP1= By.xpath("//android.widget.TextView[@text='取消']");
+    public static final By CODING= By.id("com.jianshu.haruki:id/edit_content");
+    public static final By SEARCH= By.id("com.jianshu.haruki:id/img_search");
+    public static final String CODINGTESTTEXT = "CodingTest";
+
+}
--- src/main/java/com/iappium/page/data/SendKeyData.java
+++
@@ -1,8 +0,0 @@
-package com.iappium.page.data;
-
-public class SendKeyData {
-    //搜索：昆明石林风景名胜区
-    public static final String KM = "昆明石林风景名胜区";
-    public static final CharSequence NAME = "测试";
-    public static final CharSequence PHONE = "1523835317" ;
-}
